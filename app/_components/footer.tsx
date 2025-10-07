4a879b65c24b3dc5d4565840144b0c0cd3ea5064
Povilas-Jablonskis <killeris226@gmail.com>
2025-10-07 17:04:44 +0300

    a > button

--- app/_components/footer.tsx
+++ app/_components/footer.tsx
@@ -15,88 +15,52 @@ export default function Footer() {
           <span className="text-xl font-semibold text-secondary-500">
             About
           </span>
-          <a
-            href="#"
-            className="text-base font-medium text-secondary-300 lg:text-footer"
-          >
+          <button className="text-base font-medium text-secondary-300 lg:text-footer">
             How it works
-          </a>
-          <a
-            href="#"
-            className="text-base font-medium text-secondary-300 lg:text-footer"
-          >
+          </button>
+          <button className="text-base font-medium text-secondary-300 lg:text-footer">
             Featured
-          </a>
-          <a
-            href="#"
-            className="text-base font-medium text-secondary-300 lg:text-footer"
-          >
+          </button>
+          <button className="text-base font-medium text-secondary-300 lg:text-footer">
             Partnership
-          </a>
-          <a
-            href="#"
-            className="text-base font-medium text-secondary-300 lg:text-footer"
-          >
+          </button>
+          <button className="text-base font-medium text-secondary-300 lg:text-footer">
             Bussiness Relation
-          </a>
+          </button>
         </div>
         <div>
           <span className="text-xl font-semibold text-secondary-500">
             Community
           </span>
-          <a
-            href="#"
-            className="text-base font-medium text-secondary-300 lg:text-footer"
-          >
+          <button className="text-base font-medium text-secondary-300 lg:text-footer">
             Events
-          </a>
-          <a
-            href="#"
-            className="text-base font-medium text-secondary-300 lg:text-footer"
-          >
+          </button>
+          <button className="text-base font-medium text-secondary-300 lg:text-footer">
             Blog
-          </a>
-          <a
-            href="#"
-            className="text-base font-medium text-secondary-300 lg:text-footer"
-          >
+          </button>
+          <button className="text-base font-medium text-secondary-300 lg:text-footer">
             Podcast
-          </a>
-          <a
-            href="#"
-            className="text-base font-medium text-secondary-300 lg:text-footer"
-          >
+          </button>
+          <button className="text-base font-medium text-secondary-300 lg:text-footer">
             Invite a friend
-          </a>
+          </button>
         </div>
         <div>
           <span className="text-xl font-semibold text-secondary-500">
             Socials
           </span>
-          <a
-            href="#"
-            className="text-base font-medium text-secondary-300 lg:text-footer"
-          >
+          <button className="text-base font-medium text-secondary-300 lg:text-footer">
             Discord
-          </a>
-          <a
-            href="#"
-            className="text-base font-medium text-secondary-300 lg:text-footer"
-          >
+          </button>
+          <button className="text-base font-medium text-secondary-300 lg:text-footer">
             Instagram
-          </a>
-          <a
-            href="#"
-            className="text-base font-medium text-secondary-300 lg:text-footer"
-          >
+          </button>
+          <button className="text-base font-medium text-secondary-300 lg:text-footer">
             Twitter
-          </a>
-          <a
-            href="#"
-            className="text-base font-medium text-secondary-300 lg:text-footer"
-          >
+          </button>
+          <button className="text-base font-medium text-secondary-300 lg:text-footer">
             Facebook
-          </a>
+          </button>
         </div>
       </div>
       <div className="hidden border border-[#131313]/15 lg:block"></div>
@@ -105,18 +69,12 @@ export default function Footer() {
           ©2022 MORENT. All rights reserved
         </span>
         <div className="flex justify-between gap-x-14">
-          <a
-            href="#"
-            className="text-xs font-semibold text-secondary-500 lg:text-base"
-          >
+          <button className="text-xs font-semibold text-secondary-500 lg:text-base">
             Privacy & Policy
-          </a>
-          <a
-            href="#"
-            className="text-xs font-semibold text-secondary-500 lg:text-base"
-          >
+          </button>
+          <button className="text-xs font-semibold text-secondary-500 lg:text-base">
             Terms & Condition
-          </a>
+          </button>
         </div>
       </div>
     </footer>
